7a5353f811c838e04ec22d5a6c7a017bfa3a9716
Augusto Queiroz <asqueiroz@outlook.com>
2022-02-28 12:03:14 -0300

    Criação de uma simulação de sequenciamento.

---
+++ src/SequencingSimulator.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "SequencingSimulator.h"
+#include "Hashing.h"
+#include "kmer-mapping.h"
+#include "CountMin.h"
+
+#define KMER_LENGTH 32
+#define COUNTER_MASK 0xFFFFFFFFFFFFFFF
+#define PRESENCE_THRESHOLD 25
+
+char* generateRandomSequence(size_t length) {
+    char bases[] = "ACGT";
+    char* sequence = malloc(length + 1);
+    for (size_t i = 0; i < length; i++) {
+        sequence[i] = bases[rand() % 4];
+    }
+    sequence[length] = '\0';
+    return sequence;
+}
+
+char* getRandomRead(char* sequence, size_t sequenceLength, size_t readLength) {
+    char* read = malloc(readLength + 1);
+    size_t start = rand() % (sequenceLength - readLength);
+    for (size_t i = 0; i < readLength; i++) {
+        read[i] = sequence[start + i];
+    }
+    read[readLength] = '\0';
+    return read;
+}
+
+char* getKMerStartingAt(char* read, size_t start, size_t kmerLength) {
+    char* kmer = malloc(kmerLength + 1);
+    for (size_t i = 0; i < kmerLength; i++) {
+        kmer[i] = read[start + i];
+    }
+    kmer[kmerLength] = '\0';
+    return kmer;
+}
