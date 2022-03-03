3a397e3faa13b0ffb84761b34a2157958b995679
Augusto Queiroz <asqueiroz@outlook.com>
2022-03-03 12:08:44 -0300

    fix: multiply by increment instead of adding

--- src/Hashing.c
+++ src/Hashing.c
@@ -4,7 +4,7 @@
 #include "Hashing.h"
 
 size_t incrementalFibonacciHash(size_t key, size_t hashSize, size_t increment) {
-    return ((key + increment) * 11400714819323198485llu) % hashSize;
+    return ((increment+1) * key * 11400714819323198485llu) % hashSize;
 }
 
 size_t fibonacciHash(size_t key, size_t hashSize) {
