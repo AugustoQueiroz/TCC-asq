769384e372e92427e67be2090bd05d4446affb8c
Augusto Queiroz <asqueiroz@outlook.com>
2022-03-04 16:16:09 -0300

    add: Polynomial hashing

--- src/Hashing.c
+++ src/Hashing.c
@@ -3,6 +3,21 @@
 
 #include "Hashing.h"
 
+#define LARGE_PRIME 18446744073709551557
+
+/**
+ * @brief hash(A, B, W, key) = (A * key + B) mod p mod W, where p is a large prime
+ * 
+ * @param A 
+ * @param B 
+ * @param indexSize 
+ * @param key 
+ * @return The hashing result in the range 0 to indexSize-1
+ */
+size_t polynomialHashing(size_t A, size_t B, size_t indexSize, size_t key) {
+    return (A * key + B) % LARGE_PRIME % indexSize;
+}
+
 size_t incrementalFibonacciHash(size_t key, size_t hashSize, size_t increment) {
     return ((increment+1) * key * 11400714819323198485llu) % hashSize;
 }
--- src/Hashing.h
+++ src/Hashing.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 
+size_t polynomialHashing(size_t A, size_t B, size_t indexSize, size_t key);
 size_t incrementalFibonacciHash(size_t key, size_t indexSize, size_t increment);
 size_t fibonacciHash(size_t key, size_t table_size);
 size_t fibonacciFingerprint(size_t key);
